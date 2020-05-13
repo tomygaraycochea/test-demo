fca41d8f94d3f59fc987bbdd224c8c5a11f72b97
Claudio Garaycochea <claudiogaraycochea@Claudios-MacBook-Pro.local>
2020-05-13 10:31:56 -0300

    header and modal

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,29 @@
 import React, {Component} from "react";
-import { Button, Row, Col, Form, Container } from "react-bootstrap";
+import { Button, Row, Col, Form, Container, Modal } from "react-bootstrap";
 
 class Home extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { value: ''};
+    this.state = {
+      value: '',
+      show: false,
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
     this.sendRegard = this.sendRegard.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
   }
+  
+  handleClose() {
+    this.setState({ show: false });
+  };
+
+  handleShow() {
+   this.setState({ show: true });
+  };
 
   handleChange(event) {
     this.setState({ value: event.target.value});
@@ -30,6 +43,7 @@ class Home extends Component {
   }
 
   render() {
+    const { show } = this.state;
     return (
       <Container className="">
         Home
@@ -65,6 +79,24 @@ class Home extends Component {
             <Button onClick={this.sendRegard}>Regard</Button>
           </Col>
         </Row>
+        <Button variant="primary" onClick={this.handleShow}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={show} onHide={this.handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     );  
   }
